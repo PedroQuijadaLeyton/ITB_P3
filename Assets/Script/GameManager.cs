888ae23a8d30e34261d442bdfdb0000d840ce89c
Pedro Quijada Leyton <pedro@quiley.com>
2017-07-26 17:06:35 +0100

    init

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public bool is_moving = true;
+    public GameObject recording;
+    public GameObject analyzing;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void show_recording_panel()
+    {
+        recording.SetActive(true);
+    }
+
+    public void show_analyzing_panel()
+    {
+        recording.SetActive(false);
+        analyzing.SetActive(true);
+        StartCoroutine(wait_for_sec_to_disable_analyzing());
+    }
+
+    IEnumerator wait_for_sec_to_disable_analyzing()
+    {
+        yield return new WaitForSeconds(2);
+        analyzing.SetActive(false);
+        is_moving = true;
+    }
+}
